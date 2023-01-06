2fa4e74020c64f7cad047be74915a0f0d02c5d66
RaffaJaffa <rafaloureiro99@gmail.com>
2023-01-06 11:22:23 +0000

    First Push
    Current Vehicles

---
+++ Rims/__resource.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'Beans'
+description 'Rims'
+version '3.0'
+
+files {
+    'carcols.meta',
+    'jdmrimscontentunlocks.meta'
+}
+
+
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'jdmrimscontentunlocks.meta'
---
+++ lspd/vehicle_names.lua
@@ -0,0 +1,49 @@
+Citizen.CreateThread(function()
+    -- CALLSIGNS --
+
+    AddTextEntryByHash(GetHashKey('LSPD_A0'), "Callsign - 0")
+    AddTextEntryByHash(GetHashKey('LSPD_A1'), "Callsign - 1")
+    AddTextEntryByHash(GetHashKey('LSPD_A2'), "Callsign - 2")
+    AddTextEntryByHash(GetHashKey('LSPD_A3'), "Callsign - 3")
+    AddTextEntryByHash(GetHashKey('LSPD_A4'), "Callsign - 4")
+    AddTextEntryByHash(GetHashKey('LSPD_A5'), "Callsign - 5")
+    AddTextEntryByHash(GetHashKey('LSPD_A6'), "Callsign - 6")
+    AddTextEntryByHash(GetHashKey('LSPD_A7'), "Callsign - 7")
+    AddTextEntryByHash(GetHashKey('LSPD_A8'), "Callsign - 8")
+    AddTextEntryByHash(GetHashKey('LSPD_A9'), "Callsign - 9")
+
+    AddTextEntryByHash(GetHashKey('LSPD_B0'), "Callsign - 0")
+    AddTextEntryByHash(GetHashKey('LSPD_B1'), "Callsign - 1")
+    AddTextEntryByHash(GetHashKey('LSPD_B2'), "Callsign - 2")
+    AddTextEntryByHash(GetHashKey('LSPD_B3'), "Callsign - 3")
+    AddTextEntryByHash(GetHashKey('LSPD_B4'), "Callsign - 4")
+    AddTextEntryByHash(GetHashKey('LSPD_B5'), "Callsign - 5")
+    AddTextEntryByHash(GetHashKey('LSPD_B6'), "Callsign - 6")
+    AddTextEntryByHash(GetHashKey('LSPD_B7'), "Callsign - 7")
+    AddTextEntryByHash(GetHashKey('LSPD_B8'), "Callsign - 8")
+    AddTextEntryByHash(GetHashKey('LSPD_B9'), "Callsign - 9")
+
+    AddTextEntryByHash(GetHashKey('LSPD_C0'), "Callsign - 0")
+    AddTextEntryByHash(GetHashKey('LSPD_C1'), "Callsign - 1")
+    AddTextEntryByHash(GetHashKey('LSPD_C2'), "Callsign - 2")
+    AddTextEntryByHash(GetHashKey('LSPD_C3'), "Callsign - 3")
+    AddTextEntryByHash(GetHashKey('LSPD_C4'), "Callsign - 4")
+    AddTextEntryByHash(GetHashKey('LSPD_C5'), "Callsign - 5")
+    AddTextEntryByHash(GetHashKey('LSPD_C6'), "Callsign - 6")
+    AddTextEntryByHash(GetHashKey('LSPD_C7'), "Callsign - 7")
+    AddTextEntryByHash(GetHashKey('LSPD_C8'), "Callsign - 8")
+    AddTextEntryByHash(GetHashKey('LSPD_C9'), "Callsign - 9")
+    
+    AddTextEntryByHash(GetHashKey('LSPD_LA'), "Letter A")
+    AddTextEntryByHash(GetHashKey('LSPD_LB'), "Letter B")
+    AddTextEntryByHash(GetHashKey('LSPD_LC'), "Letter C")
+
+    AddTextEntryByHash(GetHashKey('LSPD_DSH'), "Callsign - Dash")
+
+  
+    -- LIVERIES --
+
+    AddTextEntryByHash(GetHashKey('LSPD_LIV1'), "Patrol Livery")
+    AddTextEntryByHash(GetHashKey('LSPD_LIV2'), "Ghost Livery")
+
+end)
