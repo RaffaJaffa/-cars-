f814d20049163fcf6ddef5527a70cab35d550c0b
RaffaJaffa <rafaloureiro99@gmail.com>
2023-02-01 19:56:08 +0000

    LOD + Handling Update
    nis13 - Handling fix
    cullinan - Faster accel
    pgt322 - LOD Fix and handling update
    Hna2t - Suspension fix
    E63sf - Handling fix
    Jugular - Grip update
    Komoda - wheelspin fix
    Buffalo4 - accel fix
    Sugoi - accel fix
    Krieger - Grip update
    Specter - handling update
    Yosemite3 - accel fix

--- lfa/__resource.lua
+++ lfa/__resource.lua
@@ -3,14 +3,14 @@ game 'gta5'
 lua54 'true'
  
 files {
-  --  'vehiclelayouts.meta',
+    'vehiclelayouts.meta',
     'vehicles.meta',
     'carvariations.meta',
     'carcols.meta',
     'handling.meta'
 }
  
---data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
 data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
