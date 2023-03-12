297e27e1e5a3d9740ae1fa4fc4a4351b767f7110
RaffaJaffa <62546415+RaffaJaffa@users.noreply.github.com>
2023-03-12 03:12:41 +0000

    lean

--- gtavehicles/fxmanifest.lua
+++ gtavehicles/fxmanifest.lua
@@ -7,11 +7,11 @@ files {
   --  'vehicles.meta',
   --  'carcols.meta',
   --  'carvariations.meta',
-   -- 'vehiclelayouts.meta'
+   'vehiclelayouts.meta'
 }
  
 data_file 'HANDLING_FILE' 'handling.meta'
 --data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 --data_file 'CARCOLS_FILE' 'carcols.meta'
 --data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
---data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
